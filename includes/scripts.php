90d1ae24bd5c5f0f6ad4c3e505c9293aaa05125f
CharlyTheDog <jb@mediadivision.ch>
2014-09-28 00:44:48 +0200

    added main.js

--- includes/scripts.php
+++ includes/scripts.php
@@ -3,9 +3,14 @@
 
 if( !function_exists ( 'dummy_add_scripts' ) ){
 function dummy_add_scripts()
-{
-    wp_register_style('styles', get_stylesheet_directory_uri().'/assets/css/styles.css');
-	wp_enqueue_style('styles');
+{    
+    // Adding our own styles and scripts    
+    wp_register_style( 'styles', get_stylesheet_directory_uri().'/assets/css/styles.css' );
+	wp_enqueue_style( 'styles' );
+    
+    wp_register_script( 'main', get_stylesheet_directory_uri().'/assets/js/main.js' );
+	wp_enqueue_script( 'main' );
+    
 }
 add_action( 'wp_enqueue_scripts', 'dummy_add_scripts' );
 }
