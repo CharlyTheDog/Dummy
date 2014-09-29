dba24ac81434c4af7f04e8408357369c42471f48
gunfish <adriankoch@adrians-mac-pro>
2014-09-29 17:24:39 +0200

    Added 404 template

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php defined('ABSPATH') or die("Silence is golden."); 
+
+// 404 template
+?>
+
+<main role="main">
+    <?php get_template_part('templates/page', 'header'); ?> 
+    <div class="container">
+        <div class="alert alert-warning">
+            <?php _e('Sorry, but the page you were trying to view does not exist.', 'dummy'); ?>
+        </div>
+
+        <p><?php _e('It looks like this was the result of either:', 'dummy'); ?></p>
+        <ul>
+            <li><?php _e('a mistyped address', 'dummy'); ?></li>
+            <li><?php _e('an out-of-date link', 'dummy'); ?></li>
+        </ul>
+
+        <?php get_search_form(); ?>
+    </div>
+</main>
+
+
